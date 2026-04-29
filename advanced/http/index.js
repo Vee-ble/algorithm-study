fe0df96761b02361ee7d0860a75e0369cd09c18f
Vee-ble <gyflsakfn@gmail.com>
2026-04-29 10:59:16 +0900

    http 구현 연습

--- advanced/http/index.js
+++ advanced/http/index.js
@@ -10,8 +10,41 @@ server.on("connection", (socket) => {
   socket.on("data", function (data) {
     const s = data.toString();
     const r = s.slice(0, s.indexOf("\r\n"));
-    const [METHOD, PATH, VERSION] = r.split(" ");
-    console.log(METHOD, PATH, VERSION);
+    const [method, path, version] = r.split(" ");
+
+    console.log("METHOD:", method);
+    console.log("PATH:", path);
+    console.log("VERSION:", version);
+
+    let body = "";
+    let statusLine = "HTTP/1.1 200 OK";
+
+    if (method !== "GET") {
+      statusLine = "HTTP/1.1 405 Method Not Allowed";
+      body = "Method Not Allowed";
+    } else {
+      if (path === "/") {
+        body = "Home";
+      } else if (path === "/hello") {
+        body = "Hello";
+      } else {
+        statusLine = "HTTP/1.1 404 Not Found";
+        body = "Not Found";
+      }
+    }
+
+    const contentLength = Buffer.byteLength(body);
+
+    socket.write(
+      `${statusLine}\r\n` +
+        "Content-Type: text/plain; charset=utf-8\r\n" +
+        `Content-Length: ${contentLength}\r\n` +
+        "\r\n" +
+        body,
+    );
+
+    console.log(contentLength);
+    socket.end();
   });
 });
 
